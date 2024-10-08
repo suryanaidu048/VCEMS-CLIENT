08755a4f7c5d25ab687f402feebf3871f0c2e3b0
Mohan <ramgolakoti1@gmail.com>
2024-10-08 21:25:27 +0530

    api url

--- src/data/api.js
+++ src/data/api.js
@@ -1,5 +1,5 @@
 //export const API_URL = "https://vems-api.onrender.com/api/sensordata";
 //export const API_URL = "https://vems-api-liza.onrender.com/api/sensordata";
-export const API_URL = "http://13.235.68.221:4000/api/sensordata";
+export const API_URL = "http://3.111.41.152:4000/api/sensordata";
 export const API_URL2 = "https://vems-server.onrender.com";
 //export const API_URL2 = "http://localhost:3306"
