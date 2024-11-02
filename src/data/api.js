0bf5daefe0e7f56ca4c9b7032006219393f98789
Mohan <ramgolakoti1@gmail.com>
2024-11-02 16:09:17 +0530

    api url

--- src/data/api.js
+++ src/data/api.js
@@ -1,5 +1,5 @@
 //export const API_URL = "https://vems-api.onrender.com/api/sensordata";
-export const API_URL = "https://vems-api-ov0x.onrender.com/api/sensordata";
+export const API_URL = "http://15.207.115.86:4000/api/sensordata";
 //export const API_URL = "http://3.111.41.152:4000/api/sensordata";
-export const API_URL2 = "https://vems-api-ov0x.onrender.com/api1/";
+export const API_URL2 = "http://15.207.115.86:4000/api1/";
 //export const API_URL2 = "http://localhost:3306"
